9971ce251c55013ccaa4035fcd5d973dedf8fcfa
Megan Kaluza <megan.kaluza@gmail.com>
2016-10-28 11:59:47 -0700

    add functionality to update button

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   updateQuestionForm: false,
   actions: {
     update(question, params) {
-     this.sendAction('question', question, params);
+     this.sendAction('update', question, params);
    },
    delete(question) {
     if (confirm('Are you sure?')) {
