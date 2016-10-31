f061b223174cd6460a061997ed9719bafa8ce353
Megan Kaluza <megan.kaluza>
2016-10-31 06:48:29 -0700

    add functionality to the show me link

--- app/routes/index.js
+++ app/routes/index.js
@@ -32,29 +32,36 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('question');
-  },
+      return Ember.RSVP.hash({
+        questions: this.store.findAll('question')
+        // reviews: this.store.findAll('review')
+      });
+    },
+    actions: {
+      saveQuestion3(params) {
+        var newQuestion = this.store.createRecord('question', params);
+        newQuestion.save();
+        this.transitionTo('index');
+      },
+      // saveReview(params) {
+      //   console.log("in saveReview - index");
+      //   var newReview = this.store.createRecord('review', params);
+      //   newReview.save();
+      //   this.transitionTo('index');
+      // },
+      update(question, params) {
+        Object.keys(params).forEach(function(key) {
+          if(params[key]!==undefined) {
+            question.set(key,params[key]);
+          }
+        });
+        question.save();
+        this.transitionTo('index');
+      },
 
-  actions: {
-    saveQuestion3(params) {
-      var newQuestion = this.store.createRecord('question', params);
-      newQuestion.save();
+    destroyQuestion(question) {
+      question.destroyRecord();
       this.transitionTo('index');
     }
-  // 
-  //   update(question, params) {
-  //     Object.keys(params).forEach(function(key) {
-  //       if(params[key]!==undefined) {
-  //         question.set(key,params[key]);
-  //       }
-  //     });
-  //     question.save();
-  //     this.transitionTo('index');
-  //   },
-  //
-  //   destroyQuestion(question){
-  //     question.destroyRecord();
-  //     this.transitionTo('index');
-  //   }
-  // }
+  }
 });
