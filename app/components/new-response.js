3c40171616c98ce281be31478e331b22e46b129a
Megan Kaluza <megan.kaluza@gmail.com>
2016-11-01 08:08:14 -0700

    add reply button

---
+++ app/components/new-response.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewResponse: false,
+  actions: {
+    responseFormShow() {
+      this.set('addNewResponse', true);
+    },
+    saveResponse1() {
+      var params = {
+      answer: this.get('answer')
+      };
+      this.set('addNewResponse', false);
+      this.sendAction('saveResponse2', params);
+    }
+  }
+});
